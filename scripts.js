dab527dff7e76b2a35511156494702f77b24b02d
ayodada <ayotune.ad@gmail.com>
2025-08-04 22:58:04 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -37,6 +37,21 @@ document.addEventListener('DOMContentLoaded', function () {
       })
       .catch(error => console.error('Error loading blog posts:', error));
   }
+
+   document.addEventListener('DOMContentLoaded', function () {
+  // Load header
+  fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('header').innerHTML = data;
+    });
+
+  // Load footer
+  fetch('footer.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('footer').innerHTML = data;
+    });
 });
    
    
