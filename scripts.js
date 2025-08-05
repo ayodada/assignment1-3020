6315f83069f82e16d8ee61c9e5352e0c1ae54a7a
ayodada <ayotune.ad@gmail.com>
2025-08-04 22:51:27 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -15,5 +15,20 @@ document.addEventListener('DOMContentLoaded', function () {
     } else {
         console.error('Toggle button not found.');
     }
+
+
+document.addEventListener('DOMContentLoaded', function() {
+const blogList = document.getElementById('blog-list');
+fetch('../data/posts.json')
+.then(response => response.json())
+.then(posts => {
+posts.forEach(post => {
+const postElement = document.createElement('div');
+// manipulate postElement to show the content of the blog post with the
+specific style defined for it
+//add postElement as a child to blog list
 });
+})
+.catch(error => console.error('Error loading blog posts:', error));
+});      
    
